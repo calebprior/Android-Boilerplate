a3d3c2fef822573da252ca90d6f580ed9f967a2f
Caleb Prior <cprior@tcd.ie>
2017-03-03 15:55:58 +0000

    cleanup

--- app/src/main/kotlin/com/calebprior/boilerplate/BoilerplateApplication.kt
+++ app/src/main/kotlin/com/calebprior/boilerplate/BoilerplateApplication.kt
@@ -27,11 +27,11 @@ class BoilerplateApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
+            return
         }
+
         refWatcher = LeakCanary.install(this);
 
         Injector().setAsInjector()
