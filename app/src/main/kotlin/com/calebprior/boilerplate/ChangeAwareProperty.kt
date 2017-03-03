ba1c5877404bb10733493b1d44c2cc2ce9723942
Caleb Prior <cprior@tcd.ie>
2017-03-03 15:58:46 +0000

    refactor for readability

--- app/src/main/kotlin/com/calebprior/boilerplate/ChangeAwareProperty.kt
+++ app/src/main/kotlin/com/calebprior/boilerplate/ChangeAwareProperty.kt
@@ -5,14 +5,20 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 
-fun <T> changeAwareProperty(initialValue: T, onChanged: (newValue: T) -> Unit): ReadWriteProperty<Any?, T>
-        = object : ObservableProperty<T>(initialValue) {
+fun <T> changeAwareProperty(
+        initialValue: T,
+        onChanged: (newValue: T) -> Unit
+): ReadWriteProperty<Any?, T> {
 
-    override fun afterChange(property: KProperty<*>, oldValue: T, newValue: T) = onChanged(newValue)
+    return object : ObservableProperty<T>(initialValue) {
 
-    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
-        val temp = super.getValue(thisRef, property)
-        onChanged(temp)
-        return temp
+        override fun afterChange(property: KProperty<*>, oldValue: T, newValue: T)
+                = onChanged(newValue)
+
+        override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+            val value = super.getValue(thisRef, property)
+            onChanged(value)
+            return value
+        }
     }
 }
